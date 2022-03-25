efad55b9e1f69f697439cab9ff28f9a6efaa727f
asif a owadud <asifaowadud@gmail.com>
2022-03-25 23:56:24 +0600

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+// ____________ react bootstrap 5 _______________
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button,Row,Col } from 'react-bootstrap';
+
+// ____________ react bootstrap 5 _______________
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainApp">
+      
+
     </div>
   );
 }
