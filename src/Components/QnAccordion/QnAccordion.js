1985e898a2aa0f3b8a7dd02c663119eab5081b4f
asif a owadud <asifaowadud@gmail.com>
2022-03-26 15:52:55 +0600

    two question done

--- src/Components/QnAccordion/QnAccordion.js
+++ src/Components/QnAccordion/QnAccordion.js
@@ -7,15 +7,21 @@ const QnAccordion = () => {
         <div>
             <Accordion>
                 <Accordion.Item eventKey="0">
-                    <Accordion.Header>Accordion Item #1</Accordion.Header>
+                    <Accordion.Header>#1 How React Works ?</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+
+                        <p>
+                            React works generating of Virtual DOM.
+                        </p>
+
+                        <p>
+                            Directly Changing or updating Actual DOM is tough and critical for React that's reason React Generates a replica of Actual DOM which is called <strong>Virtual DOM</strong>.
+                        </p>
+
+                        <p>
+                            At first if any changes occurs in Virtual DOM then Ract starts comparing between Virtual DOM and Actual Dom by the help of <strong>Diff Algorithm</strong>. Thats why react can easily find out the place where the actual changes will take place then it just update the effect on the Actual DOM. 
+                        </p>
+
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="1">
@@ -31,15 +37,27 @@ const QnAccordion = () => {
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="2">
-                    <Accordion.Header>Accordion Item #2</Accordion.Header>
+                    <Accordion.Header>#3 How use state works ?</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                      
+                        <p>
+                            <strong>useState</strong> is a <strong>React Hook</strong> which is must use when we want any changes in our DOM state.
+                        </p>
+                      
+                        <p>
+                        <strong>Array Destructuring</strong> method is essential during declaring useState.
+                        Cause useState holds basically two type of values, 
+                        <ol>
+                            <li>the <strong>state variable</strong> to store initial value of the state &</li>
+                            <li>the <strong>setState funtion</strong> to set or update state's value as per requirement</li>
+                        </ol>
+                        </p>
+
+                        <p>
+                            And then we can use the  updated <strong>state variable</strong> for next process
+                        </p>
+
+                        <p className='text-center'><code>useState is essential to import from 'react'</code></p>
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
